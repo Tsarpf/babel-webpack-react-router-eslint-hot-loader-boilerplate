161b340b3d7fe5e7673f9329555608234121ec7a
Teemu Sarapisto <tsarpf@gmail.com>
2015-09-24 16:44:50 +0300

    Better naming

--- src/WebApp.js
+++ src/WebApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Main from './containers/application';
+import Application from './containers/application';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 
-React.render( <Main history={ createBrowserHistory() }/>, document.getElementById( 'app' ) );
+React.render( <Application history={ createBrowserHistory() }/>, document.getElementById( 'app' ) );
